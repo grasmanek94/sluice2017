eed23176dc960185429212d98ba05797f6349555
Gamer_Z <grasmanek94@gmail.com>
2017-01-07 14:51:17 +0100

    testfake

---
+++ test/SluiceTest.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <stdexcept>
+#include "gtest/gtest.h"
+
+// Als er tijd over is, tests maken met deze template
+class SluiceTest : public ::testing::Test
+{
+protected:
+	SluiceTest()
+    {
+            
+    }
+};
+
+TEST_F(SluiceTest, test_construction)
+{
+    
+}
